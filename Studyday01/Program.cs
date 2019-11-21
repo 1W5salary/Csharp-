92a59479d24b60abd2a0bf77f57e5ae2d064f21d
猕猴桃 <627664697@qq.com>
2019-11-21 19:49:02 +0800

    完成了五种参数传递的实现

--- Studyday01/Program.cs
+++ Studyday01/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using studyday01;
+using System;
 
 namespace Studyday01
 {
@@ -6,17 +7,20 @@ namespace Studyday01
     {
         public static void Main(string[] args)
         {
+
             //创建一个长度为10的栈，按进11次，弹出11次
             Stack stack = new Stack();
             stack.num = new string[10];
-
+            
+            //按进
             for (int i = 1; i <= 9;i++)
             {
                 stack.Push("学生");
             }
             stack.Push("侯滔");
             stack.Push("atai");
-
+            
+            //弹出
             for (int i = 1; i <= 11; i++)
             {
                 stack.Pop();
@@ -25,6 +29,35 @@ namespace Studyday01
 
 
 
+
+            //值类型参数的值传递
+            int age = 17;
+            grow(age);
+            int grow(int age) {
+                age++;
+                return age;
+            }
+            Console.WriteLine(age);
+            
+            //引用参数的值传递
+            Student houtao = new Student();
+            houtao.age = 24;
+            houtao.grow(houtao);
+            Console.WriteLine(houtao.age);
+            
+            //值类型的引用传递
+            Student clone = houtao;
+            clone.age = 20;
+            Console.WriteLine(houtao.age);
+
+            //引用类型的引用传递
+            houtao.refGrow(ref houtao);
+            Console.WriteLine(houtao.age);
+
+            //使用return传递参数
+            houtao.grade = 99;
+            houtao.improve(houtao);
+            Console.WriteLine(houtao.grade);
         }
     }
 }
--- Studyday01/Student.cs
+++ Studyday01/Student.cs
@@ -2,34 +2,52 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Studyday01
+namespace studyday01
 {
-    //internal class Person
-    //{
-    //    internal String name { get; set; }
-    //    internal virtual void Greet()
-    //    {
-    //        Console.WriteLine("hell body");
-    //    }
+    public class Person
+    {
+        internal string name { get; set; }
+        internal virtual void greet()
+        {
+            Console.WriteLine("hell body");
+        }
 
-    //}
-    //internal class Student : Person
-    //{
-    //    internal String major;
-    //    internal override void Greet()
-    //    {
-    //        Console.WriteLine("hell teacher");
-    //    }
+    }
+    public class Student : Person
+    {
+        public int age { get; set; }
+        public int grade { get; set; }
+        internal string major;
+        internal override void greet()
+        {
+            Console.WriteLine("hell teacher");
+        }
 
-    //}
+        public Student improve(Student student)
+        {
+            student.grade++;
+            return student;
+        }
 
-    //internal class Teacher : Person
-    //{
-    //    internal String major;
-    //    internal override void Greet()
-    //    {
-    //        Console.WriteLine("hell students");
-    //    }
+        public void grow(Student student) {
+            student.age++;
+        }
 
-    //}
+        public void refGrow(ref Student student)
+        {
+            student.age++;
+        }
+
+
+    }
+
+    internal class Teacher : Person
+    {
+        internal string major;
+        internal override void greet()
+        {
+            Console.WriteLine("hell students");
+        }
+
+    }
 }
